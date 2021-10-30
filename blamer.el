b914843d73c0a17a31a1d1fc6c7ad7ab2337a9da
artawower <artawower@mail.ru>
2021-10-30 13:34:40 +0300

    Update verison, readme.

--- blamer.el
+++ blamer.el
@@ -5,7 +5,7 @@
 ;; Author: Artur Yaroshenko <artawower@protonmail.com>
 ;; URL: https://github.com/artawower/blamer.el
 ;; Package-Requires: ((emacs "27.1"))
-;; Version: 0.2
+;; Version: 0.2.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
