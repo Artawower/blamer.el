66690395eb5271cf4a516016d514b0bf82d5388e
artawower <artawower@mail.ru>
2023-03-01 01:47:01 +0400

    chore: bump versin to 0.6 and update readme

--- blamer.el
+++ blamer.el
@@ -5,7 +5,7 @@
 ;; Author: Artur Yaroshenko <artawower@protonmail.com>
 ;; URL: https://github.com/artawower/blamer.el
 ;; Package-Requires: ((emacs "27.1") (posframe "1.1.7"))
-;; Version: 0.5.1
+;; Version: 0.6.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
