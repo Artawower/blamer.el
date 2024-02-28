11e2ffbd014f79c75f1c007d24809082c78680b6
Art <Artawower@mail.ru>
2024-02-28 22:54:53 +0300

    fix: spawning emacs buffers after async commands were interrupted (#61) (#62)

--- blamer.el
+++ blamer.el
@@ -4,8 +4,8 @@
 
 ;; Author: Artur Yaroshenko <artawower@protonmail.com>
 ;; URL: https://github.com/artawower/blamer.el
-;; Package-Requires: ((emacs "27.1") (posframe "1.1.7") (async "1.9.7"))
-;; Version: 0.8.2
+;; Package-Requires: ((emacs "27.1") (posframe "1.1.7") (async "1.9.8"))
+;; Version: 0.8.3
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -344,9 +344,6 @@ author name by left click and copying commit hash by right click.
 (defvar blamer--buffer-name "*blamer*"
   "Name of buffer for git blame messages.")
 
-(defvar blamer--async-processes '()
-  "List of currently running asynchronous processes.")
-
 (defvar-local blamer--current-author nil
   "Git.name for current repository.")
 
@@ -517,17 +514,15 @@ COMMIT-INFO - all the commit information, for `blamer--apply-bindings'"
 
 (defun blamer--async-get-email-commit-message (commit-hash callback)
   "Get email and commit message for COMMIT-HASH asynchronously.
-Rusn CALLBACK with partial user-info plist."
-  (add-to-list
-   'blamer--async-processes
-   (let ((commit-info-args (append blamer--git-commit-message (list commit-hash))))
-     (blamer--async-start
-      `(lambda ()
-         (require 'vc-git)
-         (let* ((author-email (vc-git--run-command-string nil "log" "-n1" "--pretty=format:%ae" ,commit-hash))
-                (commit-res (vc-git--run-command-string nil ,@commit-info-args)))
-           (list author-email commit-res)))
-      callback))))
+Run CALLBACK with partial user-info plist."
+  (let ((commit-info-args (append blamer--git-commit-message (list commit-hash))))
+    (blamer--async-start
+     `(lambda ()
+        (require 'vc-git)
+        (let* ((author-email (vc-git--run-command-string nil "log" "-n1" "--pretty=format:%ae" ,commit-hash))
+               (commit-res (vc-git--run-command-string nil ,@commit-info-args)))
+          (list author-email commit-res)))
+     callback)))
 
 (defun blamer--async-parse-line-info (blame-msg callback line-number &optional include-avatar-p)
   "Parse BLAME-MSG from LINE-NUMBER and create a plist with commit info.
@@ -926,13 +921,12 @@ CALLBACK will be called with result."
   (when-let*
       ((file-name file-name)
        (command (append blamer--git-blame-cmd
-                        (list (format "%s,%s" start-line end-line))))
-       (process (blamer--async-start
-                 `(lambda ()
-                    (require 'vc-git)
-                    (apply #'vc-git--run-command-string ,file-name ',command))
-                 callback)))
-    (add-to-list 'blamer--async-processes process)))
+                        (list (format "%s,%s" start-line end-line)))))
+    (blamer--async-start
+     `(lambda ()
+        (require 'vc-git)
+        (apply #'vc-git--run-command-string ,file-name ',command))
+     callback)))
 
 (defun blamer--render (&optional type)
   "Render text about current line commit status.
@@ -982,7 +976,7 @@ TYPE is optional view render type."
          (line-number start-line-number))
     (blamer--goto-line start-line-number)
     (dolist (cmd-msg commit-infos)
-      (unless (blamer--git-cmd-error-p cmd-msg)
+      (unless (and (blamer--git-cmd-error-p cmd-msg) (not (blamer--line-in-cusor-range-p line-number)))
         (blamer--async-parse-line-info
          cmd-msg
          (lambda (commit-info)
@@ -994,6 +988,14 @@ TYPE is optional view render type."
          line-number
          include-avatar-p)
         (setq line-number (1+ line-number))))))
+
+(defun blamer--line-in-cusor-range-p (line-number)
+  "Check whether current LINE-NUMBER in visible range or focused line."
+  (let ((window-start-line (line-number-at-pos (window-start)))
+        (window-end-line (line-number-at-pos (window-end))))
+    (or (and (>= line-number window-start-line)
+             (<= line-number window-end-line))
+        (eq line-number (line-number-at-pos)))))
 
 (defun blamer--goto-line (line-number)
   "Go to LINE-NUMBER."
@@ -1025,8 +1027,6 @@ Optional TYPE argument will override global `blamer-type'."
   "Render commit info with delay."
   (when blamer-idle-timer
     (cancel-timer blamer-idle-timer))
-
-  (blamer--kill-async-processes)
 
   (setq blamer-idle-timer
         (run-with-idle-timer (or blamer-idle-time 0) nil 'blamer--safety-render)))
@@ -1071,12 +1071,6 @@ LOCAL-TYPE is force replacement of current `blamer-type' for handle rendering."
       (blamer--render-commit-info-with-delay))
 
     (blamer--preserve-state)))
-
-(defun blamer--kill-async-processes ()
-  "Kill currently running async processes."
-  (dolist (process blamer--async-processes)
-    (when (process-live-p process)
-      (kill-process process))))
 
 (defun blamer--reset-state ()
   "Reset all state after blamer mode is disabled."
@@ -1150,7 +1144,7 @@ will appear after BLAMER-IDLE-TIME.  It works only inside git repo"
       (add-hook 'post-command-hook #'blamer--try-render nil t)
       (add-hook 'window-state-change-hook #'blamer--try-render nil t))))
 
-(defun blamer--async-start (start-func &optional finish-func)
+(defun blamer--async-start (start-func finish-func)
   "Optional wrapper over \\='async-start function.
 
 Needed for toggling async execution for better debug.
